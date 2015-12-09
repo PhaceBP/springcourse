da5c1d59a5b1a0f88666e9d044b5cae9e150663e
PhaceBP <attila.balogh86@gmail.com>
2015-12-09 11:14:19 +0100

    Inital commit

---
+++ src/main/java/hu/ericsson/springcourse/app/Application.java
@@ -0,0 +1,19 @@
+package hu.ericsson.springcourse.app;
+
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	
+	@SuppressWarnings("resource")
+	public static void main(String[]args){
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config/application-context.xml");
+		System.out.println("Spring container started at : "+new Date(ctx.getStartupDate()));
+		((ConfigurableApplicationContext) ctx).registerShutdownHook();
+	}
+}
